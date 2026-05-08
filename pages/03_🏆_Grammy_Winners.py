a2619e0452636cdedf1be634962208c387fcf388
Otik98 <150240603+Otik98@users.noreply.github.com>
2026-05-08 23:14:54 +0500

    Update 03_🏆_Grammy_Winners.py

--- pages/03_🏆_Grammy_Winners.py
+++ pages/03_🏆_Grammy_Winners.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="Grammy Winners",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+st.sidebar.image("assets/mj_logo.png", width=240)
+
 st.title("Grammy Winners Explorer")
 
 col1, col2, col3 = st.columns([1, 2, 1])
