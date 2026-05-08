9046277570ddb7e8086c63fe0c131058bd579a02
Otik98 <150240603+Otik98@users.noreply.github.com>
2026-05-08 23:26:32 +0500

    Update 04_🌟_This_Is_It.py

--- pages/04_🌟_This_Is_It.py
+++ pages/04_🌟_This_Is_It.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="This Is It",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+st.sidebar.image("assets/mj_logo.png", width=240)
+
+st.title("This Is It")
+
+st.markdown("Это финальная страница моего проекта.")
